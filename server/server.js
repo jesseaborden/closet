510b50088c120e565a6609b4ac100ee418f6167a
jesse <jesseaborden@gmail.com>
2016-12-17 18:17:19 -0800

    set up morgan, bodyParser, and basic index route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const PORT = 8000;
+const morgan = require('morgan');
 const sequelize = require('./database').sequelize;
+const rootRouter = require('./routers');
 
+const PORT = process.env.PORT || 8000;
 const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+app.use('/api', rootRouter);
 
 app.listen(PORT, () => console.log("Server running on port", PORT));
