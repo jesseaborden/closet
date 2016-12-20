1816736892e7f273020f0a15eba671847f8a1e02
jesse <jesseaborden@gmail.com>
2016-12-20 00:03:28 -0800

    set up basic redux structure.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App';
+import reducers from './reducers'
 import './index.css';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </ Provider>
+  , document.getElementById('root'));
